1948455b352a9ef52f3e511add2bcf07322a9d87
Haruto <101871896+haru2125@users.noreply.github.com>
2022-09-25 14:29:21 +0700

    update to v1.0.1

--- assets/js/some_code.js
+++ assets/js/some_code.js
@@ -1,15 +1,46 @@
 const loading_effect = true;
 
+const mode_normal_btn = `Switch mode <span class="beta_feature"></span>`;
+
 if (loading_effect) {
     setTimeout(function () {
         document.getElementById('load').classList.add('baymau');
     }, 2000);
 }
 
-document.getElementById('switch_font').addEventListener('click', function () {
-    if (document.getElementById('theme').classList.contains('normal-font')) {
-        document.getElementById('theme').classList.remove('normal-font');
+// document.getElementById('switch_font').addEventListener('click', function () {
+//     if (document.getElementById('theme').classList.contains('normal-font')) {
+//         document.getElementById('theme').classList.remove('normal-font');
+//     } else {
+//         document.getElementById('theme').classList.add('normal-font');
+//     }
+// });
+
+click('switch_mode', function () {
+    if (document.getElementById('theme').classList.contains('own_mode')) {
+        document.getElementById('theme').classList.remove('own_mode');
+        this.innerHTML = mode_normal_btn;
     } else {
-        document.getElementById('theme').classList.add('normal-font');
+        document.getElementById('theme').classList.add('own_mode');
+        this.innerHTML = "Go back to old mode";
     }
 });
+
+hover('badge', function () {
+    document.getElementById('badge_tooltip').classList.add('show');
+}, function () {
+    document.getElementById('badge_tooltip').classList.remove('show');
+});
+
+click('badge', function () {
+    window.open("https://discord.com/developers");
+});
+
+function hover(id, mouseenter, mouseleave) {
+    document.getElementById(id).addEventListener('mouseenter', mouseenter);
+    document.getElementById(id).addEventListener('mouseleave', mouseleave);
+}
+
+function click(id, event) {
+    document.getElementById(id).addEventListener('click', event);
+}
